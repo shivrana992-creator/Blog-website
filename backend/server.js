0728035911fec01a3f0ba752733151706d95ab16
shivrana992 <shivrana992@gmail.com>
2026-03-16 18:55:36 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const BLOGS_FILE = path.join(__dirname, 'blogs.json');
 
